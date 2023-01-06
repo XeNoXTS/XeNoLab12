200da53b7d8ccaff15b20bf2667081a7be77f6f9
XeNoXTS <palmyunvisate2003@gmail.com>
2023-01-06 15:23:15 +0700

    Lab12_1 Completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,18 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<ctime>
+
+using namespace std;
+
+int main(){
+    string grades[] = {"A","B+","B","C+","C","D+","D","F","W"};
+    srand(time(0));
+
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    cout << "You will get " << grades[rand()%9] << " in this 261102.";
+    return 0;
+}
+
